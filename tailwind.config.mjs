e02e85e28cbddc7d8a43ab6ea97eb4ce25a699e4
João Pedro <jpmrc49@gmail.com>
2024-08-27 23:22:57 -0300

    style: configures the fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,33 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Aeonik', ...defaultTheme.fontFamily.sans],
+        serif: ['Zodiak', ...defaultTheme.fontFamily.serif]
+      },
+      backgroundImage: {
+        'noise-texture': 'url(/noise-texture.png)'
+      },
+      colors: {
+        contrast: {
+          50: '#f0fdfa',
+          100: '#cbfcf3',
+          200: '#96f9e8',
+          300: '#5aeeda',
+          400: '#28d9c6',
+          500: '#0fbdad',
+          600: '#09988e',
+          700: '#0c7973',
+          800: '#0f605c',
+          900: '#11504d',
+          950: '#033030'
+        }
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
