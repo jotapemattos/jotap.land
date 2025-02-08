9a2ea2971a09de2eea6560272c49ce528c8ba7c9
João Pedro <jpmrc49@gmail.com>
2025-02-08 16:56:45 -0300

    style: lints code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,30 +1,30 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Aeonik', ...defaultTheme.fontFamily.sans],
-        serif: ['Zodiak', ...defaultTheme.fontFamily.serif]
+        sans: ["Aeonik", ...defaultTheme.fontFamily.sans],
+        serif: ["InstrumentSerif", ...defaultTheme.fontFamily.serif],
       },
       colors: {
         contrast: {
-          50: '#f0fdfa',
-          100: '#cbfcf3',
-          200: '#96f9e8',
-          300: '#5aeeda',
-          400: '#28d9c6',
-          500: '#0fbdad',
-          600: '#09988e',
-          700: '#0c7973',
-          800: '#0f605c',
-          900: '#11504d',
-          950: '#033030'
-        }
-      }
-    }
+          50: "#f0fdfa",
+          100: "#cbfcf3",
+          200: "#96f9e8",
+          300: "#5aeeda",
+          400: "#28d9c6",
+          500: "#0fbdad",
+          600: "#09988e",
+          700: "#0c7973",
+          800: "#0f605c",
+          900: "#11504d",
+          950: "#033030",
+        },
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
