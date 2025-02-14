d15daa262cf841834adfc8a04a91f466e0cbedd6
João Pedro <jpmrc49@gmail.com>
2025-02-13 21:11:28 -0300

    chore: fixes build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
+  adapter: cloudflare(),
 });
