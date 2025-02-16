d57ed7b0701eb4ac7a1a6ed5fb7ab56fb73098f4
João Pedro <jpmrc49@gmail.com>
2025-02-16 17:20:39 -0300

    refactor: changes website back to SSG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: cloudflare({ mode: "directory" }),
 });
-
